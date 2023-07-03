04675a0d02b428e680a1a4604052a5072b49cf16
MitchelMyersFl <mitchel.myers@faithlife.com>
2023-07-03 08:47:23 -0400

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,3 +3,13 @@ console.log("Hello, there.")
 setTimeout(function () {
   throw new Error("Not here.")
 }, 1000)
+
+const button = document.getElementById("mm-test-button")
+if (button) {
+  button.addEventListener("click", function () {
+    throw new Error("Not there.")
+  })
+  console.log("Button found")
+} else {
+  console.log("Button not found")
+}
